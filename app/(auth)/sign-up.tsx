2a7c37acb6dad183eeab10efa7f888412c367839
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-03-22 16:33:59 -0300

    Making the appwrite logic

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,13 @@
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '@/constants';
 import FormField from '@/components/FormField';
 import CustomButton from '@/components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { createUser } from '@/lib/appwrite';
 
-const SignUP = () => {
+const SignUp = () => {
   const [form, setForm] = useState({
     username: '',
     email: '',
@@ -15,7 +16,24 @@ const SignUP = () => {
 
   const [isSubmittin, setIsSubmittin] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error, Por favor, preencha todos os campos.');
+    }
+    setIsSubmittin(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      // set it to global state
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert('ERROR ao tentar submeter o formulário');
+      throw new Error();
+    } finally {
+      setIsSubmittin(false);
+    }
+    createUser();
+  };
   return (
     <SafeAreaView className="bg-black h-full">
       <ScrollView>
@@ -74,4 +92,4 @@ const SignUP = () => {
   );
 };
 
-export default SignUP;
+export default SignUp;
