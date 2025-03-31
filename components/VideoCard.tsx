c281ddb2af534ceb76c525b86943a649ff9b413d
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-03-31 14:19:39 -0300

    some updates

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -5,7 +5,7 @@ import { icons } from '@/constants';
 interface VideoCardProps {
   video: {
     title?: string;
-    thumbnail?: string;
+    thumbnail: string;
     video?: string;
     users: {
       username?: string;
@@ -26,7 +26,7 @@ const VideoCard = ({
     <View className="flex-col items-center px-4 pb-14">
       <View className="flex-row gap-3 items-start">
         <View className="justify-center items-center flex-row flex-1">
-          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+          <View className="w-[46px] h-[46px] justify-center items-center p-0.5">
             <Image
               source={{
                 uri: avatar,
@@ -59,19 +59,26 @@ const VideoCard = ({
         </View>
       </View>
 
-      <View>
-        {play ? (
-          <Text>Playing</Text>
-        ) : (
-          <TouchableOpacity className="w-full h-60 rounded-xl mt-3 relative justify-center items-center ">
-            <Image
-              source={{ uri: thumbnail }}
-              className="w-full h-full rounded-xl mt-3"
-              resizeMode="cover"
-            />
-          </TouchableOpacity>
-        )}
-      </View>
+      {play ? (
+        <Text className="text-white">Playing...</Text>
+      ) : (
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+          className="w-full h-60 rounded-xl mt-3 relative justify-center items-center "
+        >
+          <Image
+            source={{ uri: thumbnail }}
+            className=" h-full w-full rounded-xl mt-3 bg-white"
+            resizeMode="cover"
+          />
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
